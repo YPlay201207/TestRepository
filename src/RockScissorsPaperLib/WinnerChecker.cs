3f35b684b15e3ee1b4282772794cc45dd12d0258
YPlay <arsarapkin@mail.ru>
2020-10-18 12:32:10 +0300

    Add WinnerChecker

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -4,5 +4,18 @@ namespace RockScissorsPaperLib
 {
     public class WinnerChecker
     {
+        //1 - Rock, 2 - Paper, 3 - Scissors
+        int answer = 1;
+        public int WinnerChecker2(int FirstPlayer, int SecondPlayer){
+            answer = 1;
+            int a = FirstPlayer + 1;
+            if(a == 4){
+                a = 1;
+            }
+            if(a == SecondPlayer){
+                answer = 2;
+            }
+            return answer;
+        }   
     }
 }
