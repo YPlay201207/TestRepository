13d08cc3e3b8cb35d0354aa66f6ceea5597e0ac9
Ondushi <a_pyagay@mail.ru>
2020-10-18 14:05:26 +0300

    Add WinnerChecker3

--- src/RockScissorsPaperLib/WinnerChecker.cs
+++ src/RockScissorsPaperLib/WinnerChecker.cs
@@ -4,7 +4,7 @@ namespace RockScissorsPaperLib
 {
     public class WinnerChecker
     {
-        //1 - Rock, 2 - Paper, 3 - Scissors
+        //1 - Rock, 2 - Paper, 3 - Scissors, 4 - Lizard, 5 - Spoke 
         int answer = 1;
         public int WinnerChecker2(int FirstPlayer, int SecondPlayer){
             answer = 1;
@@ -23,5 +23,33 @@ namespace RockScissorsPaperLib
             }
             return answer;
         }   
+        public int WinnerChecker3(int FirstPlayer, int SecondPlayer)
+        {
+            switch (FirstPlayer)
+            {
+                case 1:
+                    if (SecondPlayer == 3 || SecondPlayer == 4) return 1;
+                    else if (SecondPlayer == 1) return 0;
+                    else return 2;
+                case 2:
+                    if (SecondPlayer == 1 || SecondPlayer == 5) return 1;
+                    else if (SecondPlayer == 2) return 0;
+                    else return 2;
+                case 3:
+                    if (SecondPlayer == 2 || SecondPlayer == 4) return 1;
+                    else if (SecondPlayer == 3) return 0;
+                    else return 2;
+                case 4: 
+                    if (SecondPlayer == 2 || SecondPlayer == 5) return 1;
+                    else if (SecondPlayer == 4) return 0;
+                    else return 2;
+                case 5: 
+                    if (SecondPlayer == 1 || SecondPlayer == 3) return 1;
+                    else if (SecondPlayer == 5) return 0;
+                    else return 2;
+                default:
+                    return -1;
+            }
+        }
     }
 }
