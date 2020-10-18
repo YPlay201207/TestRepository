f39abb2a133b2a0995e9568f6b0994f5b2898363
Ondushi <a_pyagay@mail.ru>
2020-10-18 13:05:37 +0300

    Add First Meth

--- src/RockScissorsPaperTests/WinnerCheckerTests.cs
+++ src/RockScissorsPaperTests/WinnerCheckerTests.cs
@@ -6,10 +6,17 @@ namespace RockScissorsPaperTests
 {
     public class WinnerCheckerTests
     {
+        const int rock = 1;
+        const int paper = 2; 
+        const int scissors = 3;
         [Fact]
-        public void Test1()
+        public void BothPlayersChooseScissors()
         {
             WinnerChecker winnerChecker = new WinnerChecker();
+            var correct = 0;
+            int result = winnerChecker.WinnerChecker2(scissors, scissors);
+            Assert.Equal(correct, result);
+
         }
     }
 }
